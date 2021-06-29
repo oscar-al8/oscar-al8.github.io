0414361641a3c4df02a6c0fd44beead710c7ea71
oscar_al8 <oscarmehrer@hotmail.com>
2021-06-29 15:47:05 -0400

    Create find me module

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,25 @@
+function copyGmail(){
+  var copyText = document.querySelector("#copy-gmail");
+  var email = copyText.innerHTML;
+  copy(email);
+}
 
+document.querySelector("#copy-gmail-click").addEventListener("click", copyGmail);
+
+function copyHotmail(){
+  var copyText = document.querySelector("#copy-hotmail");
+  var email = copyText.innerHTML;
+  copy(email);
+}
+
+document.querySelector("#copy-hotmail-click").addEventListener("click", copyHotmail);
+
+function copy(email) {
+  var createInput = document.createElement('textarea');
+  createInput.value = email;
+  document.body.appendChild(createInput);
+  createInput.select();
+  document.execCommand("copy");
+  createInput.remove();
+}
+  
