3bea123e40eaed0a32bbbe423062208e1d865ba2
oscar_al8 <oscarmehrer@hotmail.com>
2021-08-31 17:28:23 -0400

    portfolio updated

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,35 @@
 function copyGmail(){
-  var copyText = document.querySelector("#copy-gmail");
-  var email = copyText.innerHTML;
-  copy(email);
-}
-
-document.querySelector("#copy-gmail-click").addEventListener("click", copyGmail);
+    var copyText = document.querySelector("#gmail");
+    copy(copyText);
+  }
+  
+document.querySelector("#gmail").addEventListener("click", copyGmail);
 
 function copyHotmail(){
-  var copyText = document.querySelector("#copy-hotmail");
-  var email = copyText.innerHTML;
-  copy(email);
+    var copyText = document.querySelector("#hotmail");
+    copy(copyText);
 }
+  
+document.querySelector("#hotmail").addEventListener("click", copyHotmail);
 
-document.querySelector("#copy-hotmail-click").addEventListener("click", copyHotmail);
+function copy(copyText) {
 
-function copy(email) {
-  var createInput = document.createElement('textarea');
-  createInput.value = email;
-  document.body.appendChild(createInput);
-  createInput.select();
-  document.execCommand("copy");
-  createInput.remove();
-  var div = document.getElementById('alertCopy');
-  var newFirstElement = `
-      <div class="alert alert-success alert-dismissible fade show" role="alert" id='copy-alert'>
-          <strong>Email copied!</strong>
-          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div>
-  `;
-  div.innerHTML = newFirstElement;
-  setTimeout(function(){
-    div.innerHTML = '';
-  }, 3500);
+    var email = copyText.innerHTML;
+    var createInput = document.createElement('textarea');
+    createInput.value = email;
+    document.body.appendChild(createInput);
+    createInput.select();
+    document.execCommand("copy");
+    createInput.remove();
+    if(document.getElementById('alert').classList[1] !== 'show'){
+        document.getElementById('alert').classList.toggle('show');
+        setTimeout(function(){
+            document.getElementById('alert').classList.toggle('show');
+        }, 2500);
+    }
+
 }
+    
+  
+
   
